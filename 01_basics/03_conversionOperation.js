01d35987d4b0c9fd2f98f4c6c354c8d9d9b80f1c
Siddhartha51 <152762766+Siddhartha51@users.noreply.github.com>
2024-08-30 15:37:32 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,62 @@
+let score = "10abc";
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
+
+//"10" => 33
+//"10abc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn = "sid";
+let bool = Boolean(isLoggedIn)
+//console.log(bool);
+
+//1 => true; 0=> false
+//"" => false
+//"sid" => true
+
+let num = 30;
+let number = String(num);
+// console.log(number);
+// console.log(typeof number);
+
+//**************** Operations *******************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let String1 = "Siddhartha"
+let String2 = " Thapa"
+let String3 = String1 + String2
+//console.log(String3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+let gameCounter = 100
+console.log(++gameCounter);
+
+
+
+
+
+
+
+
+
+
+
