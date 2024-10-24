5a3b0241c75043292c541baf94689a1c274cc1f4
Siddhartha51 <152762766+Siddhartha51@users.noreply.github.com>
2024-10-24 16:14:10 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,31 @@
+//Array
+
+const myArry = [0,1,2,3,4]
+//console.log(myArry[2])
+
+//Array methods
+//myArry.push(10)
+//myArry.pop()
+
+//myArry.unshift(11)
+//myArry.shift()
+
+// console.log(myArry.includes(5));
+// console.log(myArry.indexOf(0));
+
+// const newArry = myArry.join()
+
+// console.log(myArry);
+// console.log(newArry)
+
+console.log("A",myArry);
+
+const myn1 = myArry.slice(1,3);
+
+console.log(myn1);
+console.log("B",myArry);
+
+const myn2 = myArry.splice(1,3);
+console.log("C", myArry);
+
+console.log(myn2);
