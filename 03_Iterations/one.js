066d74445e0db3926676ed994507a9ef684a3a90
Siddhartha51 <152762766+Siddhartha51@users.noreply.github.com>
2025-01-24 05:02:25 +0000

    iterations

---
+++ 03_Iterations/one.js
@@ -0,0 +1,22 @@
+//for
+
+// for (let sid=10; sid>1; sid--){
+//     const a = sid;
+//     if (sid == 3) {
+//         console.log("Sid is available");
+        
+//     }
+//     console.log(a);
+// }
+
+// for (let sid = 1; sid <= 10; sid++) {
+//     console.log(sid);
+//     for (let thapa = 1; thapa <= 10; thapa++){
+//         console.log(sid + '*' + thapa + ' = ' + sid*thapa);
+        
+//   }
+    
+//}
+
+// 
+
